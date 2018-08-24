aa252edf7f7bd4bcde5d5f844a8c210e446b1668
385933691 <32963374+385933691@users.noreply.github.com>
2018-08-24 08:58:30 +0800

    完善个人信息页增加拍照接口

---
+++ fun.js
@@ -0,0 +1,60 @@
+function http_request(  url , data , callback){
+    var header = {
+            "userId": getParamer('uid'),
+            "sessionId": getParamer('sessionId')
+        };
+
+    $.ajax({
+        type: "GET",
+        data: data,
+        contentType: "application/json",
+        url: url,
+        beforeSend: function (xhr) {
+            if(header){
+                for(name in header){
+                    xhr.setRequestHeader(name, header[name]);
+                }
+            }
+        },
+        success: function (data) {  
+            callback(data)
+            // return re
+        },
+    });
+
+}
+
+
+//解析url参数 start
+function getParamer(paramer){
+    var url=window.location.href.split("?")[1];           
+    if(url.indexOf("&")>0){                              
+        urlParamArry=url.split("&");                        
+        for(var i=0; i<urlParamArry.length; i++){
+            var paramerName=urlParamArry[i].split("=");  
+            if(paramer==paramerName[0]){                 
+                return paramerName[1];                     
+            }
+        }
+    }else{                                               
+        var paramerValue=url.split("=")[1];
+        return paramerValue;
+    }
+}
+
+
+//获取手机短信倒计时
+function setIntervalFun(){
+    var time = 60;
+    var loadwait = setInterval(function () {
+        time--;
+        if(time<=1){
+            clearInterval(loadwait);
+            $(".phonecodebutton").attr("onclick","getImgCode()");
+            $(".phonecodebutton").html("再次发送")
+        }else{
+            $(".phonecodebutton").attr("onclick","");
+            $(".phonecodebutton").html(time+"s")
+        }
+    },1000)
+}
